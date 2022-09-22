9645be4493ae911e4166daaf52fa65e67d55355f
Madiha <102233319+MadihaAshraf@users.noreply.github.com>
2022-09-22 13:34:44 +0500

    Add files via upload

---
+++ User.aspx
@@ -0,0 +1,67 @@
+﻿<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+
+        .auto-style1 {
+            width: 222px;
+        }
+        </style>
+</head>
+<body>
+    <form id="form2" runat="server">
+        <table>
+            <tr>
+                <td class="auto-style1">ID</td>
+                <td>
+                    <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Name</td>
+                <td>&nbsp;<asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Email</td>
+                <td>
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnInsert" runat="server" CssClass="auto-style4" OnClick="btnInsert_Click" Text="Insert" />
+                    <asp:Button ID="BtnUpdate" runat="server" OnClick="btnUpdate_Click" Text="Update" />
+                    <asp:Button ID="BtnDelete" runat="server" OnClick="btnDelete_Click" Text="Delete" />
+                    <asp:Button ID="BtnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+                </td>
+            </tr>
+        </table>
+        <asp:TextBox ID="txtMain" runat="server" CssClass="auto-style2" Height="143px" Rows="10" TextMode="MultiLine" Width="729px"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnSaveToFile" runat="server" CssClass="auto-style3" OnClick="btnSaveToFile_Click" Text="Save To File" />
+        <br />
+        <br />
+        <br />
+    </form>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ User.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Linq_to_Jason
+{
+    public partial class User : System.Web.UI.Page
+    {
+        List<Dictionary<string, object>> rows;    //creating reference of a list 
+        JavaScriptSerializer serializer = new JavaScriptSerializer();  //convert custom string into json string
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader(Server.MapPath("User.json"));
+            string json = reader.ReadToEnd();
+            rows = serializer.Deserialize<List<Dictionary<string, object>>>(json);
+            reader.Close();
+            if (!IsPostBack)
+            {
+                txtMain.Text = serializer.Serialize(rows);
+            }
+        }
+
+    
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("id", txtId.Text);
+            row.Add("name", txtName.Text);
+            row.Add("email", txtEmail.Text);
+            rows.Add(row);
+            txtMain.Text = serializer.Serialize(rows);
+          //  LoadGridView();
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("id", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            row["name"] = txtName.Text;
+            row["email"] = txtEmail.Text;
+            txtMain.Text = serializer.Serialize(rows);
+        }
+    
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("id", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            rows.Remove(row);
+            txtMain.Text = serializer.Serialize(rows);
+        }
+    
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+            row.Add("id", txtId.Text);
+            row = rows.SingleOrDefault(x => row.All(x.Contains));
+            txtName.Text = row["name"].ToString();
+            txtEmail.Text = row["email"].ToString();
+        }
+    
+
+        protected void btnSaveToFile_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter myFile = new System.IO.StreamWriter(Server.MapPath("User.json"));
+            if (!txtMain.Text.Equals(" "))
+            {
+                myFile.WriteLine(txtMain.Text);
+            }
+            myFile.Close();
+        }
+    }
+}
